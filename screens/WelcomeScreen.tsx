4003d90ce11b8f5463bbc1b7841368097847e8d2
Dissang Tenkam <dissangfrancis@yahoo.com>
2023-06-25 18:49:43 +0100

    All login interface implemented

---
+++ screens/WelcomeScreen.tsx
@@ -0,0 +1,126 @@
+import {
+    Dimensions,
+    ImageBackground,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+  } from "react-native";
+  import React from "react";
+  import Spacing from "../constants/Spacing";
+  import FontSize from "../constants/FontSize";
+  import Colors from "../constants/Colors";
+  import Font from "../constants/Font";
+  import { NativeStackScreenProps } from "@react-navigation/native-stack";
+  import { RootStackParamList } from "../types";
+  const { height } = Dimensions.get("window");
+  
+  type Props = NativeStackScreenProps<RootStackParamList, "Welcome">;
+  
+  const WelcomeScreen: React.FC<Props> = ({ navigation: { navigate } }) => {
+    return (
+      <SafeAreaView>
+        <View>
+          <ImageBackground
+            style={{
+              height: height / 2.5,
+            }}
+            resizeMode="contain"
+            source={require("../assets/images/welcome-img.png")}
+          />
+          <View
+            style={{
+              paddingHorizontal: Spacing * 4,
+              paddingTop: Spacing * 4,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: FontSize.xxLarge,
+                color: Colors.primary,
+                fontFamily: Font["poppins-bold"],
+                textAlign: "center",
+              }}
+            >
+              Bienvenue sur AutoSearchCar
+              
+            </Text>
+  
+            <Text
+              style={{
+                fontSize: FontSize.small,
+                color: Colors.text,
+                fontFamily: Font["poppins-regular"],
+                textAlign: "center",
+                marginTop: Spacing * 2,
+              }}
+            >
+             Vous pouvez explorer autant de voitures que vous le souhaitez, l'aventure sera toujours aussi belle avec nous...
+            </Text>
+          </View>
+          <View
+            style={{
+              paddingHorizontal: Spacing * 2,
+              paddingTop: Spacing * 6,
+              flexDirection: "row",
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => navigate("Login")}
+              style={{
+                backgroundColor: Colors.primary,
+                paddingVertical: Spacing * 1.5,
+                paddingHorizontal: Spacing * 2,
+                width: "48%",
+                borderRadius: Spacing,
+                shadowColor: Colors.primary,
+                shadowOffset: {
+                  width: 0,
+                  height: Spacing,
+                },
+                shadowOpacity: 0.3,
+                shadowRadius: Spacing,
+              }}
+            >
+              <Text
+                style={{
+                  fontFamily: Font["poppins-bold"],
+                  backgroundColor: Colors.primary,
+                  color: Colors.onPrimary,
+                  fontSize: FontSize.medium,
+                  textAlign: "center",
+                }}
+              >
+                S'identifier ?
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigate("Register")}
+              style={{
+                paddingVertical: Spacing * 1.5,
+                paddingHorizontal: Spacing * 2,
+                width: "48%",
+                borderRadius: Spacing,
+              }}
+            >
+              <Text
+                style={{
+                  fontFamily: Font["poppins-bold"],
+                  color: Colors.text,
+                  fontSize: FontSize.medium,
+                  textAlign: "center",
+                }}
+              >
+                S'enregistrer !
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  };
+  
+  export default WelcomeScreen;
+  
+  const styles = StyleSheet.create({});
