4e4625567b01683bdf819043f30f461216299b1b
Dissang Tenkam <dissangfrancis@yahoo.com>
2023-06-25 20:26:03 +0100

    Prelogin successfully

--- components/AppTextinput.tsx
+++ components/AppTextinput.tsx
@@ -1,50 +1,61 @@
-import {
-    StyleSheet,
-    Text,
-    TextInput,
-    TextInputProps,
-    View,
-  } from "react-native";
-  import React ,{useState} from "react";
-  import Colors from "../constants/Colors";
-  import Font from "../constants/Font";
-  import FontSize from "../constants/FontSize";
-  import Spacing from "../constants/Spacing";
-  
-  const AppTextInput: React.FC<TextInputProps> = ({ ...otherProps }) => {
-    const [focused, setFocused] = useState<boolean>(false);
-    return (
-      <TextInput
-        onFocus={() => setFocused(true)}
-        onBlur={() => setFocused(false)}
-        placeholderTextColor={Colors.darkText}
-        style={[
-          {
-            fontFamily: Font["poppins-regular"],
-            fontSize: FontSize.small,
-            padding: Spacing * 2,
-            backgroundColor: Colors.lightPrimary,
-            borderRadius: Spacing,
-            marginVertical: Spacing,
-          },
-          focused && {
-            borderWidth: 3,
-            borderColor: Colors.primary,
-            shadowOffset: { width: 4, height: Spacing },
-            shadowColor: Colors.primary,
-            shadowOpacity: 0.2,
-            shadowRadius: Spacing,
-          },
-        ]}
-        {...otherProps}
-      />
-    );
-  };
-  
-  export default AppTextInput;
-  
-  const styles = StyleSheet.create({});
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, TextInputProps, View } from "react-native";
+import Colors from "../constants/Colors";
+import Font from "../constants/Font";
+import FontSize from "../constants/FontSize";
+import Spacing from "../constants/Spacing";
 
-// function useState<T>(arg0: boolean): [any, any] {
-//   throw new Error("Function not implemented.");
-// }
+interface AppTextInputProps extends TextInputProps {
+  insertNumber?: boolean;
+}
+
+const AppTextInput: React.FC<AppTextInputProps> = ({ insertNumber, ...otherProps }) => {
+  const [focused, setFocused] = useState<boolean>(false);
+  const [text, setText] = useState<string>("");
+
+  const handleChangeText = (inputText: string) => {
+    if (insertNumber) {
+      // Remove non-digit characters from the input text
+      const cleanedText = inputText.replace(/[^0-9]/g, "");
+      setText(cleanedText);
+    } else {
+      setText(inputText);
+    }
+  };
+
+  return (
+    <TextInput
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      placeholderTextColor={Colors.darkText}
+      style={[
+        styles.input,
+        focused && styles.focusedInput,
+      ]}
+      value={text}
+      onChangeText={handleChangeText}
+      {...otherProps}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    fontFamily: Font["poppins-regular"],
+    fontSize: FontSize.small,
+    padding: Spacing * 2,
+    backgroundColor: Colors.lightPrimary,
+    borderRadius: Spacing,
+    marginVertical: Spacing,
+  },
+  focusedInput: {
+    borderWidth: 3,
+    borderColor: Colors.primary,
+    shadowOffset: { width: 4, height: Spacing },
+    shadowColor: Colors.primary,
+    shadowOpacity: 0.2,
+    shadowRadius: Spacing,
+  },
+});
+
+export default AppTextInput;
--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,123 +1,158 @@
-import {
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
-  } from "react-native";
-  import React from "react";
-  import Spacing from "../constants/Spacing";
-  import FontSize from "../constants/FontSize";
-  import Colors from "../constants/Colors";
-  import Font from "../constants/Font";
-  import { Ionicons } from "@expo/vector-icons";
-  import { NativeStackScreenProps } from "@react-navigation/native-stack";
-  import { RootStackParamList } from "../types";
-  import AppTextInput from "../components/AppTextinput";
-  
-  type Props = NativeStackScreenProps<RootStackParamList, "Register">;
-  
-  const RegisterScreen: React.FC<Props> = ({ navigation: { navigate } }) => {
-    return (
-      <SafeAreaView>
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import axios from 'axios';
+
+import Spacing from "../constants/Spacing";
+import FontSize from "../constants/FontSize";
+import Colors from "../constants/Colors";
+import Font from "../constants/Font";
+import { Ionicons } from "@expo/vector-icons";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types";
+import AppTextInput from "../components/AppTextinput";
+
+type Props = NativeStackScreenProps<RootStackParamList, "Register">;
+
+const RegisterScreen: React.FC<Props> = ({ navigation: { navigate } }) => {
+  const [nom, setNom] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [numero_telephone, setnumero_telephone] = useState("");
+
+  const registerVisitor = async () => {
+    try {
+      const data = {
+        nom: nom,
+        email: email,
+        password: password,
+        numero_telephone: numero_telephone
+      };
+
+      const response = await axios.post('https://fast-sands-15969-99650e82dc8c.herokuapp.com/visiteurs', data);
+
+      console.log(response.data); // Vous pouvez traiter la réponse de l'API ici
+
+      // Naviguez vers une autre page ou effectuez une autre action après l'enregistrement du visiteur
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      <View
+        style={{
+          padding: Spacing * 2,
+        }}
+      >
         <View
           style={{
-            padding: Spacing * 2,
+            alignItems: "center",
           }}
         >
-          <View
+          <Text
             style={{
-              alignItems: "center",
-            }}
-          >
-            <Text
-              style={{
-                fontSize: FontSize.xLarge,
-                color: Colors.primary,
-                fontFamily: Font["poppins-bold"],
-                marginVertical: Spacing * 3,
-              }}
-            >
-              Créer un compte
-            </Text>
-            <Text
-              style={{
-                fontFamily: Font["poppins-regular"],
-                fontSize: FontSize.small,
-                maxWidth: "80%",
-                textAlign: "center",
-              }}
-            >
-              vous devez creér un compte pour etre en mesure de visiter nos véhicules
-            </Text>
-          </View>
-          <View
-            style={{
+              fontSize: FontSize.xLarge,
+              color: Colors.primary,
+              fontFamily: Font["poppins-bold"],
               marginVertical: Spacing * 3,
             }}
           >
-            <AppTextInput placeholder="adresse Mail" />
-            <AppTextInput placeholder="Mot de passe" />
-            <AppTextInput placeholder="Confirme l'adresse Mail" />
-          </View>
-  
-          <TouchableOpacity
+            Créer un nouveau compte
+          </Text>
+          <Text
             style={{
-              padding: Spacing * 2,
-              backgroundColor: Colors.primary,
-              marginVertical: Spacing * 3,
-              borderRadius: Spacing,
-              shadowColor: Colors.primary,
-              shadowOffset: {
-                width: 0,
-                height: Spacing,
-              },
-              shadowOpacity: 0.3,
-              shadowRadius: Spacing,
+              fontFamily: Font["poppins-regular"],
+              fontSize: FontSize.small,
+              maxWidth: "80%",
+              textAlign: "center",
             }}
           >
-            <Text
-              style={{
-                fontFamily: Font["poppins-bold"],
-                color: Colors.onPrimary,
-                textAlign: "center",
-                fontSize: FontSize.large,
-              }}
-            >
-              Sign up
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => navigate("Login")}
-            style={{
-              padding: Spacing,
-            }}
-          >
-            <Text
-              style={{
-                fontFamily: Font["poppins-semiBold"],
-                color: Colors.text,
-                textAlign: "center",
-                fontSize: FontSize.small,
-              }}
-            >
-              Vous avez déja un compte ?
-            </Text>
-          </TouchableOpacity>
-  
-          <View
-            style={{
-              marginVertical: Spacing * 3,
-            }}
-          >
-           
-  
-           
-          </View>
+            Vous devez créer un compte pour pouvoir explorer les véhicules disponibles chez nous.
+          </Text>
         </View>
-      </SafeAreaView>
-    );
-  };
-  
-  export default RegisterScreen;
+        <View
+          style={{
+            marginVertical: Spacing * 3,
+          }}
+        >
+          <AppTextInput
+            placeholder="Nom"
+            value={nom}
+            onChangeText={setNom}
+          />
+          <AppTextInput
+            placeholder="Adresse email"
+            value={email}
+            onChangeText={setEmail}
+          />
+          <AppTextInput
+            placeholder="Mot de passe"
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+          />
+          <AppTextInput
+            placeholder="Numéro de téléphone"
+            insertNumber={true}
+            value={numero_telephone}
+            onChangeText={setnumero_telephone}
+          />
+        </View>
+
+        <TouchableOpacity
+          onPress={registerVisitor}
+          style={{
+            padding: Spacing * 2,
+            backgroundColor: Colors.primary,
+            marginVertical: Spacing * 3,
+            borderRadius: Spacing,
+            shadowColor: Colors.primary,
+            shadowOffset: {
+              width: 0,
+              height: Spacing,
+            },
+            shadowOpacity: 0.3,
+            shadowRadius: Spacing,
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: Font["poppins-bold"],
+              color: Colors.onPrimary,
+              textAlign: "center",
+              fontSize: FontSize.large,
+            }}
+          >
+            Créer
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigate("Login")}
+          style={{
+            padding: Spacing,
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: Font["poppins-semiBold"],
+              color: Colors.text,
+              textAlign: "center",
+              fontSize: FontSize.small,
+            }}
+          >
+            Vous avez déjà un compte ?
+          </Text>
+        </TouchableOpacity>
+
+        <View
+          style={{
+            marginVertical: Spacing * 3,
+          }}
+        ></View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default RegisterScreen;
