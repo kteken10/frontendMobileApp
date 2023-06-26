7c17514e4b994619ad839a013c82050ed00bb29c
Dissang Tenkam <dissangfrancis@yahoo.com>
2023-06-26 09:11:08 +0100

    Getting Data Card

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,23 @@
-import React from "react";
-import { StyleSheet, SafeAreaView, View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, SafeAreaView, View, Text, ScrollView } from "react-native";
 import SearchBar from "../components/SearchBar";
+import CarAutomobile from "../components/CarAutomobile";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../types";
+import axios from "axios";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Home">;
+
+interface VehicleData {
+  id: number;
+  marque: string;
+  prix: number;
+  type_vehicule: string;
+  couleur: string;
+  duree: string;
+  fournisseur_id: number;
+  image: any;
+}
 
 const HomeScreen: React.FC<Props> = ({ navigation }) => {
   const handleSearch = (searchCriteria: any) => {
@@ -12,15 +25,43 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
     console.log(searchCriteria);
   };
 
+  const [vehicleData, setVehicleData] = useState<VehicleData[]>([]);
+
+  useEffect(() => {
+    fetchVehicleData();
+  }, []);
+
+  const fetchVehicleData = async () => {
+    try {
+      const response = await axios.get("http://your-api-url.com/automobiles");
+      const data = response.data;
+      setVehicleData(data);
+    } catch (error) {
+      console.log("Erreur lors de la récupération des données du véhicule :", error);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>AutoSearchCar</Text>
       </View>
-      <View style={styles.content}>
+      <ScrollView style={styles.content}>
         <SearchBar onSearch={handleSearch} />
-        {/* Autre contenu de l'écran */}
-      </View>
+        {vehicleData.map((vehicle, index) => (
+          <CarAutomobile
+            key={index}
+            image={vehicle.image}
+            nom={vehicle.marque}
+            fournisseur={vehicle.fournisseur_id.toString()}
+            prix={vehicle.prix}
+            onPress={() => {
+              console.log(`Clic sur le véhicule : ${vehicle.marque}`);
+              // Autres actions à exécuter lors du clic sur la carte de véhicule
+            }}
+          />
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
