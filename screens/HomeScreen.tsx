00879811571cb3e95a6d1d085fbaa2df1c0c668a
Dissang Tenkam <dissangfrancis@yahoo.com>
2023-06-27 03:47:35 +0100

    RAS

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -10,12 +10,13 @@ type Props = NativeStackScreenProps<RootStackParamList, "Home">;
 
 const HomeScreen: React.FC<Props> = ({ navigation }) => {
   const [vehicleData, setVehicleData] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     const fetchVehicleData = async () => {
       try {
-        const response = await axios.get("https://fast-sands-15969-99650e82dc8c.herokuapp.com/automobiles"); // Remplacez l'URL par l'endpoint approprié de votre API
-        setVehicleData(response.data); // Stocke les données des véhicules dans l'état
+        const response = await axios.get("https://fast-sands-15969-99650e82dc8c.herokuapp.com/automobiles");
+        setVehicleData(response.data);
       } catch (error) {
         console.log(error);
       }
@@ -24,10 +25,15 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
     fetchVehicleData();
   }, []);
 
-  const handleSearch = (searchCriteria: any) => {
-    // Effectuer une action en fonction des critères de recherche
-    console.log(searchCriteria);
-
+  const handleSearch = async (searchCriteria: any) => {
+    try {
+      const response = await axios.get("https://fast-sands-15969-99650e82dc8c.herokuapp.com/recherche", {
+        params: searchCriteria,
+      });
+      setSearchResults(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -38,20 +44,35 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
       <View style={styles.content}>
         <SearchBar onSearch={handleSearch} />
         <ScrollView>
-          {vehicleData.map((vehicle, index) => (
-            <CarAutomobile
-              key={index}
-              image={{ uri: vehicle.image }}
-              nom={vehicle.marque}
-              fournisseur={vehicle.fournisseur_id}
-              prix={vehicle.prix}
-              onPress={() => {
-                console.log(`Clic sur le véhicule : ${vehicle.marque}`);
-
-                navigation.navigate("CarAutomobileDetails",{vehicleData: vehicle});
-              }}
-            />
-          ))}
+          {searchResults.length === 0 ? (
+            vehicleData.map((vehicle, index) => (
+              <CarAutomobile
+                key={index}
+                image={{ uri: vehicle.image }}
+                nom={vehicle.marque}
+                fournisseur={vehicle.fournisseur_info.nom_fournisseur}
+                prix={vehicle.prix}
+                onPress={() => {
+                  console.log(`Clic sur le véhicule : ${vehicle.marque}`);
+                  navigation.navigate("CarAutomobileDetails", { vehicleData: vehicle });
+                }}
+              />
+            ))
+          ) : (
+            searchResults.map((vehicle, index) => (
+              <CarAutomobile
+                key={index}
+                image={{ uri: vehicle.image }}
+                nom={vehicle.marque}
+                fournisseur={vehicle.fournisseur_info.nom_fournisseur}
+                prix={vehicle.prix}
+                onPress={() => {
+                  console.log(`Clic sur le véhicule : ${vehicle.marque}`);
+                  navigation.navigate("CarAutomobileDetails", { vehicleData: vehicle });
+                }}
+              />
+            ))
+          )}
         </ScrollView>
       </View>
     </SafeAreaView>
